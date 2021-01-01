b35b6d80c23d39cf5325a1b7e74236d90628ee65
Arpit Patel <arpit@temi.ca>
2021-01-01 14:43:41 -0500

    Initial commit

---
+++ app/src/main/java/com/hoemtech/rossetup/MainActivity.java
@@ -0,0 +1,20 @@
+package com.hoemtech.rossetup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.ros.android.view.RosTextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // For Test of the ROS 
+    RosTextView rosTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
